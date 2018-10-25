6bfd9c9e958f5b511babc0b642f1fb13181d6b9b
autodocs <autodocs>
2018-10-25 22:40:01 +0000

    build based on 84eba9a

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Launcher",
+    "title": "Launcher",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Launcher-1",
+    "page": "Launcher",
+    "title": "Launcher",
+    "category": "section",
+    "text": "Documentation goes here."
+},
+
+]}
