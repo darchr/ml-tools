c2bcd6b7b2e93fe2d8ab899382732d5fa9dd0a27
autodocs <autodocs>
2018-10-31 19:36:27 +0000

    build based on 6400406

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "ML-Tools",
     "title": "ML-Tools",
     "category": "section",
-    "text": "Documentation on the ML-Tools repo*"
+    "text": "This repo contains a collection of tools, containers, and scripts for launching machine learning workloads in Docker containers and collecting statistics about the  containers."
 },
 
 {
@@ -33,7 +33,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "notebooks.html#[Basic-Analysis](https://github.com/darchr/ml-tools/blob/master/notebooks/basic_analysis/basic_analysis.ipynb)-1",
+    "location": "notebooks.html#[Basic-Analysis](https://github.com/darchr/ml-notebooks/blob/master/basic_analysis/basic_analysis.ipynb)-1",
     "page": "Notebooks",
     "title": "Basic Analysis",
     "category": "section",
@@ -41,11 +41,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "notebooks.html#[CPU-Analysis](https://github.com/darchr/ml-tools/blob/master/notebooks/cpu_analysis/cpu_analysis.ipynb)-1",
+    "location": "notebooks.html#[CPU-Analysis](https://github.com/darchr/ml-notebooks/blob/master/cpu_analysis/cpu_analysis.ipynb)-1",
     "page": "Notebooks",
     "title": "CPU Analysis",
     "category": "section",
     "text": "Plots and some analysis of how the memory requirements and training speed for 2 epochs of  training scale as the number of available processors is increased."
+},
+
+{
+    "location": "docker.html#",
+    "page": "Docker",
+    "title": "Docker",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "docker.html#Docker-1",
+    "page": "Docker",
+    "title": "Docker",
+    "category": "section",
+    "text": "Docker images are used to create reproducible environments and to more easily enable tricks like CPU and memory limiting."
 },
 
 {
@@ -61,63 +77,95 @@ var documenterSearchIndex = {"docs": [
     "page": "Tensorflow",
     "title": "Tensorflow",
     "category": "section",
-    "text": "We will use Tensorflow as one of the ML frameworks for  testing. Since the standard distribution for Tensorflow is not compiled with AVX2  instructions, I compiled Tensorflow from source on amarillo. The directory tf-compile/ has the relevant files for how this is done.The Docker Hub where the most current version of this container lives is here: https://hub.docker.com/r/darchr/tf-compiled-base/. This repo will be kept  up-to-date as I make needed changes to the container.The next few sections will detail the steps taken to compile Tensorflow as well as the other containers used."
+    "text": "We will use Tensorflow as one of the ML frameworks for  testing. Since the standard distribution for Tensorflow is not compiled with AVX2  instructions, I compiled Tensorflow from source on amarillo. The directory tf-compile/ has the relevant files for how this is done.The Docker Hub where the most current version of this container lives is here: https://hub.docker.com/r/darchr/tf-compiled-base/. This repo will be kept  up-to-date as I make needed changes to the container.I\'m using the official tensorflow docker approach to compile and build the pip package for tensor flow.https://www.tensorflow.org/install/source\nhttps://www.tensorflow.org/install/dockerHelpful post talking about docker permissions https://denibertovic.com/posts/handling-permissions-with-docker-volumes/"
 },
 
 {
-    "location": "tf-compiled-base.html#",
-    "page": "Building Tensorflow",
-    "title": "Building Tensorflow",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "tf-compiled-base.html#Building-Tensorflow-1",
-    "page": "Building Tensorflow",
-    "title": "Building Tensorflow",
-    "category": "section",
-    "text": "I\'m using the official tensorflow docker approach to compile and build the pip package for tensor flow.https://www.tensorflow.org/install/source\nhttps://www.tensorflow.org/install/dockerHelpful post talking about docker permissions https://denibertovic.com/posts/handling-permissions-with-docker-volumes/"
-},
-
-{
-    "location": "tf-compiled-base.html#Compilation-Overview-1",
-    "page": "Building Tensorflow",
+    "location": "tensorflow.html#Compilation-Overview-1",
+    "page": "Tensorflow",
     "title": "Compilation Overview",
     "category": "section",
-    "text": "Containers will be build incrementally, starting with darchr/tf-compiled-base, which is the base image containing Tensorflow that has been compiled on amarillo. Compiling Tensorflow is important because the default Tensorflow binary is not compiled to use AVX2 instructions. Using the very scientific \"eyeballing\" approach, this compiled version of Tensorflow runs ~60% faster."
+    "text": "Containers will be build incrementally, starting with darchr/tf-compiled-base, which is the base image containing Tensorflow that has been compiled on amarillo. Compiling Tensorflow is important because the default Tensorflow binary is not compiled to use AVX2 instructions. Using the very scientific \"eyeballing\" approach, this compiled version of Tensorflow runs ~60% faster.Other containers that use Tensorflow can be build from darchr/tf-compiled/base."
 },
 
 {
-    "location": "tf-compiled-base.html#Building-tf-compiled-base-1",
-    "page": "Building Tensorflow",
+    "location": "tensorflow.html#Building-tf-compiled-base-1",
+    "page": "Tensorflow",
     "title": "Building tf-compiled-base",
     "category": "section",
     "text": "As a high level overview, we use an official Tensorflow docker containers to build a  Python 3.5 \"wheel\" (package). We then use a Python 3.5.6 docker container as a base to  install the compiled tensorflow wheel."
 },
 
 {
-    "location": "tf-compiled-base.html#Compiling-Tensorflow-1",
-    "page": "Building Tensorflow",
+    "location": "tensorflow.html#Compiling-Tensorflow-1",
+    "page": "Tensorflow",
     "title": "Compiling Tensorflow",
     "category": "section",
-    "text": "Pull the docker container with the source code:docker pull tensorflow/tensorflow:nightly-devel-py3Launch the container withdocker run -it -w /tensorflow -v $PWD:/mnt -e HOST_PERMS=\"$(id -u):$(id -g)\" tensorflow/tensorflow:nightly-devel-py3 bashThis does the following:Opens the container in the /tensorflow directory, which contains the tensorflow source   code\nMounts the current directory into the /mnt directory in the container. This allows the   .whl build to be dropped in the PWD after compilation.Inside the container, rungit pullto pull the latest copy of the tensorflow source. Then configure the build with./configure # I just used all defaults for nowSteps to build:bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package\n./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt\nchown $HOST_PERMS /mnt/tensorflow-1.11.0rc1-cp35-cp35m-linux_x86_64.whlNote, compilation takes quite a while, so be patient. If running on amarillo, enjoy the 96 thread awesomeness."
+    "text": "Pull the docker container with the source code:docker pull tensorflow/tensorflow:1.10.0-devel-py3Launch the container withdocker run -it -w /tensorflow -v $PWD:/mnt -e HOST_PERMS=\"$(id -u):$(id -g)\" tensorflow/tensorflow:1.10.0-devel-py3 bashThis does the following:Opens the container in the /tensorflow directory, which contains the tensorflow source   code\nMounts the current directory into the /mnt directory in the container. This allows the   .whl build to be dropped in the PWD after compilation.Inside the container, rungit pullto pull the latest copy of the tensorflow source. Then configure the build with./configureSettings used:Python Location: default\nPython Library Path: default\njemalloc support: Y\nGoogle cloud platform support: n\nHadoop file system support: n\nAmazon AWS platform support: n\nApache Kafka Platform support: n\nXLA Jis support: N\nGDR support: N\nVERBs support: N\nnGraph support: N\nOpenCL SYCL support: N\nCUDA support: N\nFresh clang release: N\nMPI support: N\nOptimization flags: default\nInteractively configure ./WORKSPACE: NSteps to build:bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package\n./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt\nchown $HOST_PERMS /mnt/tensorflow-1.10.1-cp35-cp35m-linux_x86_64.whlNote, compilation takes quite a while, so be patient. If running on amarillo, enjoy the 96 thread awesomeness."
 },
 
 {
-    "location": "tf-compiled-base.html#Summary-1",
-    "page": "Building Tensorflow",
+    "location": "tensorflow.html#Summary-1",
+    "page": "Tensorflow",
     "title": "Summary",
     "category": "section",
-    "text": "docker pull tensorflow/tensorflow:nightly-devel-py3\ndocker run -it -w /tensorflow -v $PWD:/mnt -e HOST_PERMS=\"$(id -u):$(id -g)\" tensorflow/tensorflow:nightly-devel-py3 bash\n# inside container\ngit pull\n./configure # I just used all defaults for now\nbazel build --config=opt //tensorflow/tools/pip_package:build_pip_package\n./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt\nchown $HOST_PERMS /mnt/tensorflow-1.11.0rc1-cp35-cp35m-linux_x86_64.whl"
+    "text": "docker pull tensorflow/tensorflow:nightly-devel-py3\ndocker run -it -w /tensorflow -v $PWD:/mnt -e HOST_PERMS=\"$(id -u):$(id -g)\" tensorflow/tensorflow:nightly-devel-py3 bash\n# inside container\ngit pull\n./configure # Look at options above\nbazel build --config=opt //tensorflow/tools/pip_package:build_pip_package\n./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt\nchown $HOST_PERMS /mnt/tensorflow-1.10.1-cp35-cp35m-linux_x86_64.whl"
 },
 
 {
-    "location": "tf-compiled-base.html#Building-tf-compiled-base-2",
-    "page": "Building Tensorflow",
-    "title": "Building tf-compiled-base",
+    "location": "tensorflow.html#Building-the-Docker-Image-1",
+    "page": "Tensorflow",
+    "title": "Building the Docker Image",
     "category": "section",
-    "text": "With the .whl for tensorflow build, we can build a new Docker container with this  installed. For this step, move tensorflow-...-.whl into the tf-compiled-base/  directory.Annoyingly, the .whl created in the previous step only works with Python 3.5. I tried  hacking it by changing the name (cp35-cp35m -> cp36-cp36m), but installation with pip  failed. This means that we need a working copy of Python 3.5 in order to run this.  Fortunately, the Python foundation supplies Debian (I think ... or Ubuntu) based containers for past Python versions. We can use this as a starting point for our Dockerfile.The Dockerfile is pretty self-explanatory. The one tricky bit is that the  tensorflow .whl built in the previous step must be linked to the container so we can  install the compiled tensorflow. You can use the build script as./build.sh tensorflow-1.11.0rc1-cp35-cm35m-linux_x86_64.whlor run the docker command directly asdocker build -t darchr/tf-compiled-base . --build-arg tensorflow=tensorflow-1.11.0rc1-cp35-cp35m-linux_x86_64.whl"
+    "text": "With the .whl for tensorflow build, we can build a new Docker container with this  installed. For this step, move tensorflow-...-.whl into the tf-compiled-base/  directory. Then, run the shell script:./build.sh tensorflow-1.10.1-cp35-cm35m-linux_x86_64.whlFinally, if necessary, push the image to the darchr docker hub viadocker push darchr/tf-compiled-base"
+},
+
+{
+    "location": "tensorflow.html#Details-1",
+    "page": "Tensorflow",
+    "title": "Details",
+    "category": "section",
+    "text": "Annoyingly, the .whl created in the previous step only works with Python 3.5. I tried  hacking it by changing the name (cp35-cp35m -> cp36-cp36m), but installation with pip  failed. This means that we need a working copy of Python 3.5 in order to run this.  Fortunately, the Python foundation supplies Debian (I think ... or Ubuntu) based containers for past Python versions. We can use this as a starting point for our Dockerfile.Permissions with the docker containers was becoming a bit of a nightmare. I finally found a solution that works by installing gosu:https://github.com/tianon/gosu\nhttps://denibertovic.com/posts/handling-permissions-with-docker-volumes/Essentially, a dummy account user is created that does not have root privileges, but we can still create directories within the docker containers."
+},
+
+{
+    "location": "tensorflow.html#Building-tf-keras-1",
+    "page": "Tensorflow",
+    "title": "Building tf-keras",
+    "category": "section",
+    "text": "Just run the build script with:./build.sh"
+},
+
+{
+    "location": "keras.html#",
+    "page": "Keras Models",
+    "title": "Keras Models",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "keras.html#Keras-Models-1",
+    "page": "Keras Models",
+    "title": "Keras Models",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "keras.html#Cifar-Cnn-1",
+    "page": "Keras Models",
+    "title": "Cifar Cnn",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "keras.html#Resnet-Cnn-1",
+    "page": "Keras Models",
+    "title": "Resnet Cnn",
+    "category": "section",
+    "text": "TODO"
 },
 
 {
