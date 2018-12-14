ec28b03d191785cb9343fc2bab348c7ccd9f4bb9
hildebrandmw <mhildebrand@ucdavis.edu>
2018-12-14 14:17:31 -0800

    Add common build environment

---
+++ env/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Fetch git repos
+git clone https://github.com/hildebrandmw/PAPI.jl PAPI
+git clone https://github.com/hildebrandmw/DockerX.jl DockerX
+git clone https://github.com/hildebrandmw/MemSnoop.jl MemSnoop
+git clone https://github.com/hildebrandmw/SnoopAnalyzer.jl SnoopAnalyzer
+git clone https://github.com/darchr/ml-notebooks
