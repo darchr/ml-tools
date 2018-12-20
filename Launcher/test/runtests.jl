1831a39b61a18c3fec28272fa54ba3d4ad081c87
Mark Hildebrand <mhildebrand@ucdavis.edu>
2018-12-20 13:17:46 -0800

    Remove old docker containers

--- Launcher/test/runtests.jl
+++ Launcher/test/runtests.jl
@@ -5,5 +5,6 @@ const ROOT = joinpath(@__DIR__, "..", "..")
 const DOCKER = joinpath(ROOT, "docker")
 
 include("utils.jl")
+# Remove since I moved everything to a single docker container
 #include("docker.jl")
 include("workloads.jl")
