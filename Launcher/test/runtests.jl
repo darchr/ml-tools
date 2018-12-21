7105f1d7f37d01bc2bdfdda1f119927ba9618e46
Mark Hildebrand <mhildebrand@ucdavis.edu>
2018-12-21 13:25:55 -0800

    Pull needed docker images

--- Launcher/test/runtests.jl
+++ Launcher/test/runtests.jl
@@ -1,8 +1,14 @@
 using Launcher
 using Test
+using DockerX
 
 const ROOT = joinpath(@__DIR__, "..", "..")
 const DOCKER = joinpath(ROOT, "docker")
+
+# Pull the needed images
+@info "Pulling Needed Docker Images"
+DockerX.pull_image("ubuntu:latest")
+DockerX.pull_image("darchr/tf-compiled-base:latest")
 
 include("utils.jl")
 # Remove since I moved everything to a single docker container
