b51e16380b9e940a31d3f214c59fcb73ff6c51d5
Mark Hildebrand <mhildebrand@ucdavis.edu>
2019-02-01 21:25:59 +0000

    I don't even know anymore

--- workloads/rnn_translator/docker/build.sh
+++ workloads/rnn_translator/docker/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-=======
 
 # Need to navigate to the src directory in order to include the code.
->>>>>>> c4f4f8adba18c015b92cb5c5af500e85684560ac
 cd ../src/pytorch
 docker build -t "darchr/gnmt" --rm -f ../../docker/Dockerfile .
