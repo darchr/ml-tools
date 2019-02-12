63828b4c916efe39d2d7f9acb392b341ac5cd15e
Mark Hildebrand <mhildebrand@ucdavis.edu>
2019-02-12 01:00:10 +0000

    Update deps

--- deps/init.sh
+++ deps/init.sh
@@ -4,5 +4,6 @@
 git clone https://github.com/hildebrandmw/PAPI.jl PAPI
 git clone https://github.com/hildebrandmw/Docker.jl Docker
 git clone https://github.com/hildebrandmw/SystemSnoop.jl SystemSnoop
+git clone https://github.com/hildebrandmw/PCM.jl PCM
 
 julia --project=. -e "using Pkg; Pkg.build()"
